bfba0089bd9eb254528363966f8e4503b3e76a87
Rinn <svs050705@gmail.com>
2025-07-14 08:28:12 +0400

    feat: setted up structure

---
+++ settings.gradle.kts
@@ -0,0 +1,35 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "RickAndMortyApp"
+include(":app")
+include(":feature")
+include(":data")
+include(":feature:main")
+include(":feature:character")
+include(":feature:settings")
+include(":feature:language")
+include(":feature:main:characters")
+include(":feature:main:filters")
+include(":data:characters")
+include(":data:characters:remote")
+include(":data:characters:local")
+include(":data:settings")
